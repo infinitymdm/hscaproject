ffa19a87e7f70781eb84ebb9d0adadd26ebf6d55
Marcus Mellor <infinitymdm@gmail.com>
2023-03-28 12:26:52 -0500

    Add HDL for flopenr plus stub for round_f32

---
+++ synthDC/hdl/flopenr.sv
@@ -0,0 +1,12 @@
+module flopenr #(parameter WIDTH = 32)
+   (input  logic clk, enable, reset,
+    input  logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q);
+
+    always_ff @(posedge clk, posedge reset)
+        if (reset)
+            q <= 0;
+        else if (enable)
+            q <= d;
+
+endmodule
---
+++ synthDC/hdl/round.sv
@@ -0,0 +1,5 @@
+module round_f32 (input logic x[31:0], // Also need to decide how many guard bits to have
+                  input logic mode,
+                  output logic y[31:0]);
+    assign y = x; // TODO
+endmodule
