f20b96ab72b24767a06c0319e612bd6e287905aa
Marcus Mellor <infinitymdm@gmail.com>
2023-04-04 13:05:54 -0500

    WIP Initial attempt at using provided test vectors

--- hdl/tb.sv
+++ hdl/tb.sv
@@ -1,17 +1,30 @@
 module tb ();
 
-    logic [31:0] dividend, divisor, quotient;
+    int fd; // file descriptor
+    int fstatus;
+    string line;
 
+    logic [1:0]  round_mode;
+    logic [31:0] dividend, divisor, expected_quotient, quotient;
+
+    // Initialize device under test
     fpdiv dut(dividend, divisor, quotient);
 
     initial begin
-        #10;
-        dividend = 32'h8683F7FF;
-        #10;
-        divisor  = 32'hC07F3FFF;
-        #10;
-        $display("q expected = 05845B44");
-        $display("q          = %h", quotient);
+        fd = $fopen("../fptests/vectors/f32_div_rne.tv", "r");
+        $display("       N |        D |        Q");
+        $display("------------------------------");
+        while (!$feof(fd)) begin
+            fstatus = $fgets(line, fd); // Read in a test vector
+            $display("%s", line);
+            fstatus = $sscanf(line, "%8h_%8h_%8h_%2b", dividend, divisor, expected_quotient, round_mode);
+            $write("%h | %h | %h \t ", dividend, divisor, quotient);
+            if (quotient !== expected_quotient)
+                $display("Fail! Expected %h", expected_quotient);
+            else
+                $display("Ok");
+        end
+        $fclose(fd);
     end
 
 endmodule
