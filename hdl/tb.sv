f832b5f092f950b2596a9e74017bda33deaeaf12
Marcus Mellor <infinitymdm@gmail.com>
2023-04-05 08:27:10 -0500

    Show pass/fail counts in testbench

--- hdl/tb.sv
+++ hdl/tb.sv
@@ -1,7 +1,7 @@
 module tb ();
 
-    int fd; // file descriptor
-    int fstatus;
+    int fd, fstatus; // file descriptor
+    int num_pass, num_fail;
     string line;
 
     logic [1:0]  round_mode;
@@ -11,23 +11,28 @@ module tb ();
     fpdiv dut(dividend, divisor, quotient);
 
     initial begin
+        num_pass = 0;
+        num_fail = 0;
         fd = $fopen("../fptests/vectors/f32_div_rne.tv", "r");
         $display("       N |        D |        Q");
         $display("------------------------------");
         while (!$feof(fd)) begin
             fstatus = $fgets(line, fd); // Read in a test vector
-            $display("%s", line);
             fstatus = $sscanf(line, "%8h_%8h_%8h_%2b", dividend, divisor, expected_quotient, round_mode);
             #10;
             $write("%h | %h | %h \t ", dividend, divisor, quotient);
             if (quotient !== expected_quotient) begin
                 $display("Fail! Expected %h", expected_quotient);
+                num_fail++;
             end
             else begin
                 $display("Ok");
+                num_pass++;
             end
         end
         $fclose(fd);
+        $display("Passed: %d tests", num_pass);
+        $display("Failed: %d tests", num_fail);
         $finish;
     end
 
