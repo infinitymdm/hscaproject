d34a11a0392fc23b6caf464d89b465fbbd762e9a
Marcus Mellor <infinitymdm@gmail.com>
2023-07-10 12:36:08 -0500

    Minor fix to exp_op

--- synthDC/hdl/exponent_op.sv
+++ synthDC/hdl/exponent_op.sv
@@ -7,12 +7,13 @@ module exponent_op #(parameter WIDTH=8) (
 );
     logic [WIDTH-1:0] bias = 2**(WIDTH-1)-1;
     logic [WIDTH-1:0] e; // local for e before subtracting decrement
-    always_comb begin
+    always_comb
         case (op)
             00: {e, shift} = {(e1 - e2) + bias, 1'b0};
             01: {e, shift} = e1 + bias;
+            default: {e, shift} = {WIDTH{1'bx}}
         endcase
-        e3 = e - decrement;
-    end
+
+    assign e3 = e - decrement;
 
 endmodule
